42a3a1707462fea46e20b80e49649849338bad3d
Anton <61093173+ghtx280@users.noreply.github.com>
2024-01-02 22:28:08 +0200

    test local apis

--- frontend/src/routes/api/test/+server.js
+++ frontend/src/routes/api/test/+server.js
@@ -2,8 +2,13 @@ import { json } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
+
+    let url = "https://yogafaynyy.com/api/v1/course"
+
+    let aaa = await fetch(url)
+    aaa = aaa.json()
     
     return json({
-        message: "Hello, world!"
+        message: aaa
     });
 }
