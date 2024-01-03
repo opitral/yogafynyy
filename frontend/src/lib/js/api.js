7b8959e3c9cf902a4846a52d9c4ccc20054e3ea7
Anton <61093173+ghtx280@users.noreply.github.com>
2024-01-03 15:03:18 +0200

    test fix all local endpoints

--- frontend/src/lib/js/api.js
+++ frontend/src/lib/js/api.js
@@ -1,5 +1,6 @@
 import Rapi from "./fetch.js";
 
 export const api = Rapi(fetch, {
-    default: "https://yogafaynyy.com/api/v1/" 
+    // default: "https://yogafaynyy.com/api/v1/" 
+    default: "http://127.0.0.1:4000/api/v1/" 
 })
