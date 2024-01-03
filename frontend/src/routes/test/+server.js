b85682e400bcf1097e1bf8e8b466342fedc3e9d3
Anton <61093173+ghtx280@users.noreply.github.com>
2024-01-03 14:17:05 +0200

    0

--- frontend/src/routes/test/+server.js
+++ frontend/src/routes/test/+server.js
@@ -3,7 +3,8 @@ import { json } from '@sveltejs/kit';
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
 
-    let url = "https://yogafaynyy.com/api/v1/course"
+    // let url = "https://yogafaynyy.com/api/v1/course"
+    let url = "http://127.0.0.1:4000/api/v1/course"
 
     let aaa
 
