d37472d53603cf39edf9fdb15fc7e06cae476315
Anton <61093173+ghtx280@users.noreply.github.com>
2024-01-03 14:31:40 +0200

    test rapi and fetch

--- frontend/src/routes/test/+server.js
+++ frontend/src/routes/test/+server.js
@@ -4,17 +4,63 @@ import Rapi from '$lib/js/fetch.js';
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
 
-    // let url = "https://yogafaynyy.com/api/v1/course"
-    let url = "http://127.0.0.1:4000/api/v1/course"
+    let url1 = "https://yogafaynyy.com/api/v1/course"
+    let url2 = "http://127.0.0.1:4000/api/v1/course"
 
     let api = Rapi()
+    
+    async function test1() {
+        let { data, error } = await api.get.json(url1)
+        return { data, error }
+    }
 
-    let { data, error } = await api.get.json(url)
+    async function test2() {
+        let { data, error } = await api.get.json(url2)
+        return { data, error }
+    }
+
+    async function test3() {
+        try {
+            let response = await fetch(url1)
+            return {
+                data: await response.json(),
+                error: null
+            }
+        } catch (error) {
+            return {
+                data: null,
+                error: error
+            }
+        }
+    }
+
+    async function test4() {
+        try {
+            let response = await fetch(url2)
+            return {
+                data: await response.json(),
+                error: null
+            }
+        } catch (error) {
+            return {
+                data: null,
+                error: error
+            }
+        }
+    }
 
 
     return json({
-        message: data,
-        error: error
+        tests: {
+            rapi: {
+                local: await test1(),
+                domen: await test2()
+            },
+            fetch: {
+                local: await test3(),
+                domen: await test4()
+            }
+        }
     });
 }
 
