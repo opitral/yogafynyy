85c23aa277a5740f2c72cd4ee66a8e55f0008ff6
Anton <61093173+ghtx280@users.noreply.github.com>
2024-01-03 14:26:43 +0200

    test rapi

--- frontend/src/routes/test/+server.js
+++ frontend/src/routes/test/+server.js
@@ -1,4 +1,5 @@
 import { json } from '@sveltejs/kit';
+import Rapi from '$lib/js/fetch.js';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
@@ -6,16 +7,32 @@ export async function GET() {
     // let url = "https://yogafaynyy.com/api/v1/course"
     let url = "http://127.0.0.1:4000/api/v1/course"
 
-    let aaa
+    let api = Rapi()
 
-    try {
-        aaa = await fetch(url)
-        aaa = await aaa.json()
-    } catch (error) {
-        aaa = error + ""
-    }
-    
+    let { data, error } = await api.get.json(url)
+
+
     return json({
-        message: aaa
+        message: data,
+        error: error
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // try {
+    //     aaa = await fetch(url)
+    //     aaa = await aaa.json()
+    // } catch (error) {
+    //     aaa = error + ""
+    // }
