3a857ab76732d95b5c0aad893aac37f863c1105c
anton99999 <an280502@gmail.com>
2024-02-08 18:18:08 +0200

    fix modal close

--- frontend/src/lib/ui/FormModal.svelte
+++ frontend/src/lib/ui/FormModal.svelte
@@ -1,25 +1,19 @@
 <script>
-    import { page       } from "$app/stores";
-    import { store      } from "$lib/store.js";
-    import { onMount    } from "svelte";
-    import { fade, fly  } from "svelte/transition";
-    import { alertError } from "$lib/js/tst-variants.js";
-    import { api        } from "$lib/js/api.js";
-
+    import { page       } from '$app/stores';
+    import { store      } from '$lib/store.js';
+    import { onMount    } from 'svelte';
+    import { fade, fly  } from 'svelte/transition';
+    import { alertError } from '$lib/js/tst-variants.js';
+    import { api        } from '$lib/js/api.js';
 
     export let price = 0, discount = 0, new_price = 0;
 
-    price /= 100
-    new_price /= 100
+    price /= 100;
+    new_price /= 100;
 
+    const fly_params = { y: 100, duration: 300 };
 
-    $: ok = $page.url.hash == "#buy"
-
-    const fly_params = {
-        y: 100,
-        duration: 300
-    }
-
+    let open;
     let body;
     let input_email;
     let input_phone;
@@ -27,38 +21,45 @@
     let mask_phone;
     let disabled;
 
-    $: if (body) {
-        body.style.overflow = ok ? "hidden" : ""
+    function hashChange(hash) {
+        open = hash === '#buy';
     }
 
-    $: if (ok && body && input_phone) {
+    $: hashChange($page.url.hash);
+
+    $: if (body) {
+        body.style.overflow = open ? 'hidden' : '';
+    }
+
+    $: if (open && body && input_phone) {
         // mask_email = new Inputmask('email').mask(input_email);
         mask_phone = new Inputmask('+38 (999) 99-99-999').mask(input_phone);
     }
-   
+
     onMount(() => {
-        body = document.body
-    })
+        body = document.body;
+        addEventListener('hashchange', () => hashChange(location.hash));
+    });
 
     async function submitForm(e) {
-        if (disabled) return
+        if (disabled) return;
 
-        disabled = true
+        disabled = true;
 
-        const { data, error } = await api.post.json("course/buy", {
+        const { data, error } = await api.post.json('course/buy', {
             body: {
                 email: $store.email,
                 phone: mask_phone.unmaskedvalue(),
-            }
-        })
+            },
+        });
 
-        disabled = false
+        disabled = false;
 
-        if (error) { 
+        if (error) {
             if (error.type) {
                 error.cnt = {
-                    error: "Помилка. Спробуйте пізніше"
-                }
+                    error: 'Помилка. Спробуйте пізніше',
+                };
             }
 
             return alertError.show({
@@ -67,14 +68,13 @@
             });
         }
 
-        // open(data.message); 
-        
-        location.href = data.message
-    }    
-        
+        // open(data.message);
+
+        location.href = data.message;
+    }
 </script>
 
-{#if ok}
+{#if open}
     <div class="modal" transition:fade>
         <form
             action="#"
@@ -84,7 +84,15 @@
             on:submit|preventDefault={submitForm}
         >
             <div class="close">
-                <button type="button" class="bg-tp pointer" text="24" on:click={()=>history.back()}>✕</button>
+                <button
+                    type="button"
+                    class="bg-tp pointer"
+                    text="24"
+                    on:click={() => {
+                        history.back();
+                        // open = false
+                    }}>✕</button
+                >
             </div>
 
             <p text="20 bold" class="w-95%">
@@ -94,33 +102,27 @@
                 Ціна курсу {new_price} грн
                 <span text="14 gray delete">{price} грн</span>
             </p>
-            <p text="20 bold">
-                Вкажіть свої данні
-            </p>
+            <p text="20 bold">Вкажіть свої данні</p>
 
             <input
                 required
-                type="text" 
-                name="email" 
-                placeholder="Ваш E-mail"  
+                type="text"
+                name="email"
+                placeholder="Ваш E-mail"
                 bind:this={input_email}
                 bind:value={$store.email}
-            >
+            />
             <input
                 required
                 type="tel"
                 name="phone"
                 placeholder="Ваш Телефон"
                 bind:this={input_phone}
-            >
-            
-            <button
-                class="btn disabled:op-20"
-                type="submit"
-                
-                
-                {disabled}
-            >Далі</button>
+            />
+
+            <button class="btn disabled:op-20" type="submit" {disabled}>
+                Далі
+            </button>
         </form>
     </div>
 {/if}
@@ -128,7 +130,7 @@
 <style>
     form {
         position: relative;
-        background: var(--black); 
+        background: var(--black);
         padding: 30px;
         border: 1px solid var(--brand50);
     }
@@ -136,7 +138,7 @@
     input {
         width: 100%;
         padding: 15px;
-        background: var(--black); 
+        background: var(--black);
         border: 1px solid var(--brand);
     }
 
@@ -146,7 +148,7 @@
         height: 100%;
         top: 0;
         left: 0;
-        background: rgba(0, 0, 0, .5);
+        background: rgba(0, 0, 0, 0.5);
         display: flex;
         justify-content: center;
         align-items: center;
